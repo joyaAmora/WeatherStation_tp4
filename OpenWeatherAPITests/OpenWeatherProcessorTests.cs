f274c82ede560d1642f848219afe997d9cbb83e5
Jade <jade.phaneuf@gmail.com>
2020-11-09 11:27:25 -0500

    changé les dossiers d'intégrations pour les copier avec la solution

---
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OpenWeatherAPITests
+{
+    public class OpenWeatherProcessorTests
+    {
+        //Afficher le message de l’exception dans l’erreur que ApiKey est vide ou null.
+        public void GetOneCallAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+
+        }
+        //Afficher le message de l’exception dans l’erreur que ApiKey est vide ou null.
+        public void GetCurrentWeatherAsync_IfApiKeyEmptyOrNull_ThrowArgumentExceptionn()
+        {
+
+        }
+        //Afficher le message de l’exception dans l’erreur que le client http n’est pas initialisé.
+        public void GetOneCallAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+
+        }
+        //Afficher le message de l’exception dans l’erreur que le client http n’est pas initialisé
+        public void GetCurrentWeatherAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+
+        }
+    }
+}
